eb34c1f5781eff1c3c9a71ca796dc294fde424e7
mSousaa <matheussousa851@gmail.com>
2025-03-27 17:14:19 -0300

    add-login-screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; 
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
-
-    <div class="parent">
-        <div class="div1">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        </div>
-        <div className="div2">
-                <div className="login-card">
-                    <h2>Login</h2>
-                    <input type="email" placeholder="Email" className="input-field" />
-                    <input type="password" placeholder="Senha" className="input-field" />
-                    <button className="login-button">Entrar</button>
-                </div>
-            </div>
+    <div className="parent">
+      <div className="div1">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <div className="div2">
+        <Login />
+      </div>
     </div>
   );
 }
