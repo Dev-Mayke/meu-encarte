1f70716faf5f5a4eb4dc174e7a1ab7113d12f77a
mSousaa <matheussousa851@gmail.com>
2025-04-01 08:25:42 -0300

    add-searchbar

--- src/pages/painel.js
+++ src/pages/painel.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './painel.css';
 import logo from '../logo.svg';
+import { SearchBar } from '../components/SearchBar';
+import { SearchResultsList } from '../components/SearchResultList';
+
 
 function Painel() {
     const handleClick = () => {
         window.location.href = "/encartes"; 
       };
       
+const [results, setResults] = useState([])
+
   return (
     <div className="parent">
       <div className="headerPainel">
         <img src={logo} className="App-logo" alt="logo" />
         </div>
       <div className="div2">Conteúdo Principal
-        <div className='search-bar-container'>
-        <div>SearchBar</div>
-        <div>SearchResults</div>
-        </div>
+      <div className="search-bar-container">
+        <SearchBar setResults={setResults} />
+        {results && results.length > 0 && <SearchResultsList results={results} />}
+      </div>
       </div>
       <div className="div4">
         <button className="next-button" onClick={handleClick}>Próximo</button>
